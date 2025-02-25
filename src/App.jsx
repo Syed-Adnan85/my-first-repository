a29f823d256e5cd8326037ad190bd785d69ff784
Syed Adnan <adnansami8522@gmail.com>
2025-02-25 11:54:23 +0530

    new button in feature-1

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,7 @@ export default function App() {
               >
                 ✖
               </button>
-              <button>for feature-1 branch</button>
+              <button>its feature-1 branch</button>
             </li>
           ))}
         </ul>
